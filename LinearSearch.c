6c42206ca93525f7733e53a86fa4f93ce5c2f565
amritkarki001 <sudhirr4657@gmail.com>
2024-02-26 10:05:25 +0545

    added

---
+++ LinearSearch.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <conio.h>
+
+int LinearSearch(int l[],int k,int N)
+{
+	int i;
+	
+	for(i=0;i<N; i++)
+	{
+		if(l[i]==k)
+		{
+			printf("The Number %d is Found ",k);
+			return 1;
+		}
+	}
+	printf("\n Number %d is Not Found",k);
+	return 0;
+}
+
+void main()
+{
+	int l[20]={2,4,5,8,9,10};
+	LinearSearch(l,10,20);
+	getch();
+	
+}
