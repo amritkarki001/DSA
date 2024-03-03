6123926d6468db514af0b7397fae8cd06909cfff
amritkarki001 <sudhirr4657@gmail.com>
2024-03-03 11:08:31 +0545

    error

--- shell_sort.c
+++ shell_sort.c
@@ -31,7 +31,36 @@ void main()
 }
 */
 
-void shellsort(int A[], int N)
+void shellsort(int A[50], int N)
+{
+	int gap,i,j,tmp;
+	for (gap=N/2; gap>0;gap/=2)
+	{
+		for (j=gap; j<N; j++)
+		{
+			tmp=A[j];
+			for(i=j; i>=gap && A[i-gap]>tmp; i=i-gap)
+			{
+				A[i]=A[i-gap];
+			}
+			A[i]=tmp;
+		}
+	}
+}
+void main()
+{
+	int A[50]={23,29,15,19,31,7,9,5,2};
+	int i;
+	shellsort(A,10);
+	for(i=1;i<10;i++)
+	{
+		printf("After Sort %d ",A[i]);
+	}
+
+}
+
+/*
+void shellsort(int A[50], int N)
 {
 	int gap,i,j,tmp;
 	for (gap=N/2; gap>0;gap/=2)
@@ -56,5 +85,5 @@ void main()
 	{
 		printf(" %d ",A[i]);
 	}
-	getch();
-}
+
+}*/
