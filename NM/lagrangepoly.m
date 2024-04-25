0a699c4ab484df729746f9db8d1bb7de1f96e5ea
amritkarki001 <sudhirr4657@gmail.com>
2024-04-25 06:26:00 +0545

    added

--- NM/lagrangepoly.m
+++ NM/lagrangepoly.m
@@ -1,42 +1,16 @@
-% Lagrange Interpolation MATLAB Program
- 
-function [P,R,S] = lagrangepoly(X,Y,XX)
-X = [0 20 40 60 80 100]; % inputting the values of given x
-Y = [26.0 48.6 61.6 71.2 74.8 75.2]; % inputting the values of given y
-%[P,R,S] = lagrangepoly(X,Y);
- 
-xx = 0.5 : 0.01 : 6.5; 
-%plot(xx,polyval(P,xx),X,Y,'or',R,S,'.b',xx,spline(X,Y,xx),'--g')
- 
-%grid
- 
-%axis([0.5 8.5 -5 5])
- 
-if size(X,1) > 1;  X = X'; end % checking for parameters 
-if size(Y,1) > 1;  Y = Y'; end
-if size(X,1) > 1 || size(Y,1) > 1 || size(X,2) ~= size(Y,2)
-  error('both inputs must be equal-length vectors') % displaying error
-end % end of scope of if
-N = length(X);
-pvals = zeros(N,N);
-% for evaluating  the polynomial weights for each order
- 
-for i = 1:N
-  % the polynomial with roots may be values of X other than this one
-  pp = poly(X( (1:N) ~= i));
-  pvals(i,:) = pp ./ polyval(pp, X(i));
+x = [0 20 40 60 80 100];
+y = [26.0 48.6 61.6 71.2 74.8 75.2];
+sum = 0;
+a = 55;
+for i = 1:length(x)
+ u = 1;
+ l = 1;
+ for j = 1:length(x)
+ if j ~= i
+ u = u * (a - x(j));
+ l = l * (x(i) - x(j));
+ end
+ end
+ sum= sum + u / l * y(i);
 end
-P = Y*pvals;
-if nargin==3
-  YY = polyval(P,XX); % output is YY with given XX
-  P = YY; % assigning to output
-end
-% end of scope of if 
- 
-if nargout > 1 % checking for conndtions 
-  R = roots( ((N-1):-1:1) .* P(1:(N-1)) );
-  if nargout > 2
-    % the evalustion of acual value at  the poins of zero derivative  
-    S = polyval(P,R);
-  end
-end
+disp(sum);
